2ee727612f240379613be63c817bbb345bab7b1f
victormatia <fvictor1705@gmail.com>
2025-08-20 22:03:00 -0300

    feat: cria rota para resetar os planos

--- src/app/api/payments/subscribe/route.ts
+++ src/app/api/payments/subscribe/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { Preference } from "mercadopago";
 import { prisma } from "@/lib/prisma";
 import mercadoPagoConfig from "@/lib/mercadopago";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 export type TSubscribeResponde = {
   preferenceId: string;
@@ -12,9 +14,6 @@ export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    console.log('body', body)
-
-    // valida pagador
     const payer = await prisma.user.findFirst({
       where: { id: body.payerId, isActive: true },
       include: {
@@ -22,12 +21,9 @@ export async function POST(req: Request) {
       },
     });
 
-    console.log('payer', payer)
-
     if (!payer)
       return NextResponse.json({ message: "Payer Not Found" }, { status: 404 });
 
-    // valida plano
     const enterprisePlan = await prisma.plan.findFirst({
       where: { name: "Enterprise", isActive: true },
     });
@@ -35,7 +31,6 @@ export async function POST(req: Request) {
     if (!enterprisePlan)
       return NextResponse.json({ message: "Invalid Plan" }, { status: 400 });
 
-    // cria preferencia
     const preference = new Preference(mercadoPagoConfig);
 
     const { id, init_point } = await preference.create({
@@ -65,7 +60,7 @@ export async function POST(req: Request) {
             name: payer.name,
             email: payer.email,
             providerId: payer.serviceProvider?.id,
-            userId: payer.id
+            userId: payer.id,
           },
         },
         external_reference: enterprisePlan.id,
@@ -95,7 +90,59 @@ export async function POST(req: Request) {
   }
 }
 
+export async function PATCH() {
+  try {
+    const session = await getServerSession(authOptions);
 
-// coisas para fazer
-// 1. verificar pq a rota de buscar dados do usuário logado está vindo os dados do admin
-// 2. finalizar a lógica de criação do pagamento via webhook
+    if (!session?.user?.id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const serviceProvider = await prisma.serviceProvider.findFirst({
+      where: { userId: session.user.id },
+      include: {
+        subscriptions: { where: { status: "ACTIVE" } },
+      },
+    });
+
+    if (serviceProvider?.subscriptions.length) {
+      await Promise.all(
+        serviceProvider?.subscriptions.map(async (sub) => {
+          return await prisma.subscription.update({
+            where: { id: sub.id },
+            data: {
+              isAutoRenew: false,
+              status: "EXPIRED",
+            },
+          });
+        })
+      );
+
+      const startPlan = await prisma.plan.findFirst({
+        where: { name: "Start" },
+      });
+
+      await prisma.serviceProvider.update({
+        where: { id: serviceProvider.id },
+        data: {
+          planId: startPlan?.id,
+          subscriptions: {
+            update: {
+              where: { id: startPlan?.id },
+              data: {
+                status: 'ACTIVE',
+              }
+            },
+          },
+        },
+      });
+    }
+
+    return NextResponse.json({ message: 'Reset Subscriptions' }, { status: 200 });
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "Internal Server Error", details: error.message },
+      { status: 500 }
+    );
+  }
+}
