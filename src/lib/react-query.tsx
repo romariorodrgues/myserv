143c11763850eebb86e4adb3f86914ed23947f75
Matheus <matheusfoaraujo.dev@gmail.com>
2025-09-25 07:22:32 -0300

    fix react-query

--- src/lib/react-query.tsx
+++ src/lib/react-query.tsx
@@ -1,15 +1,31 @@
 'use client'
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import dynamic from 'next/dynamic'
+import React from 'react'
 
-export const queryClient = new QueryClient()
+const ReactQueryDevtools =
+  process.env.NODE_ENV === 'development'
+    ? dynamic(
+        () =>
+          import('@tanstack/react-query-devtools').then(
+            (mod) => mod.ReactQueryDevtools,
+          ),
+        { ssr: false },
+      )
+    : () => null
 
-export  default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const [queryClient] = React.useState(() => new QueryClient())
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  );
+  )
 }
